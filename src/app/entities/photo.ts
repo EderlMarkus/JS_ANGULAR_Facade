df1051ec0f4dbfc4d7ca3b6c1e1cf330a1d27cff
Markus Ederl <markus.ederl@r-software.at>
2023-05-23 10:25:14 +0200

    Auto-generated commit [ci skip]

--- src/app/entities/photo.ts
+++
@@ -1,7 +0,0 @@
-export interface Photo {
-  albumId: string;
-  id: string;
-  title: string;
-  url: string;
-  thumbnailUrl: string;
-}
--- src/app/entities/photoSearchParams.ts
+++
@@ -1,9 +0,0 @@
-export interface PhotoSearchParams{
-  id: string,
-  albumId: string;
-}
-
-export const intialPhotoSearchParams: PhotoSearchParams = {
-  id: "",
-  albumId: "",
-}
